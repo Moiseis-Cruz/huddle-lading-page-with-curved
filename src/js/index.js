4fdffa5cc79a95d633c3891500082e8e62f48119
Moiseis-Cruz <moizzito@gmail.com>
2023-01-23 19:40:56 -0300

    Está feito - não se do jeito correto.

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,32 @@
 const form = document.getElementById("form");
 const inputEmail = document.getElementById("email");
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    checkInputs()
+})
+
+
+function checkInputs() {
+    const inputEmailValue = inputEmail.value.trim()
+
+    if(inputEmailValue === "") {
+        errorValidation(inputEmail, "Check your email please")
+    }else{
+        successValidation(inputEmail)
+    }
+}
+
+function errorValidation(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector("small")
+
+    small.innerText = message
+
+    formControl.className = "form-control error"
+}
+
+function successValidation(){
+    alert("Você está inscrito!!")
+}
